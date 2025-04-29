169665c28f568b00a9c57c0bdd62dcaa917e75f2
gautamkumar1 <gautamraaz936@gmail.com>
2025-04-29 17:18:59 +0530

    Dark mode added and navbar

---
+++ client/components/hero-section.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import 'swiper/css'
+import 'swiper/css/autoplay'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/effect-coverflow'
+import Navbar from './Navbar'
+export default function HeroSection() {
+    
+
+    return (
+        <>
+            <header>
+                <Navbar/>
+            </header>
+        </>
+    )
+}
+
