00c9d9147ee38a482fae0e48e3ae7aacd7f7eaa9
gautamkumar1 <gautamraaz936@gmail.com>
2025-05-01 12:50:43 +0530

    Fixed env issue

--- client/apis/apis.ts
+++ client/apis/apis.ts
@@ -1,7 +1,9 @@
 import { WaitlistData } from "@/types/waitlist-type";
 import axios from "axios";
 const BACKEND_URL = process.env.NODE_ENV === "production"
-? process.env.PROD_BACKEND_URL : process.env.DEV_BACKEND_URL;
+? process.env.NEXT_PUBLIC_PROD_BACKEND_URL : process.env.NEXT_PUBLIC_DEV_BACKEND_URL;
+console.log(BACKEND_URL, "BACKEND_URL");
+
 const axiosInstance = axios.create({
   baseURL: BACKEND_URL,
   withCredentials: true,
