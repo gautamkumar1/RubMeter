ed9f71a127e4502bce06a608315562187db8f671
gautamkumar1 <gautamraaz936@gmail.com>
2025-05-01 12:49:52 +0530

    Cors setup done

--- server/src/server.js
+++ server/src/server.js
@@ -3,8 +3,17 @@ import express from "express"
 import waitlistRoutes from "./routes/waitlist-routes.js";
 import rateLimiterMiddleware from "./middlewares/rateLimiter-middleware.js";
 import { connectDB } from "./database/db.config.js";
+import cors from "cors";
 dotenv.config()
 const app = express()
+const FRONTEND_URL = process.env.NODE_ENV === "development" ? process.env.DEV_FRONTEND_URL : process.env.PROD_FRONTEND_URL
+const corsOptions = {
+  origin: [FRONTEND_URL], 
+  methods: "GET,POST,PUT,DELETE", 
+  allowedHeaders: "Content-Type,Authorization", 
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
